aaa213c28486911687f091f8a3251cb5a302f4ae
sajidhassan15 <88380034+sajidhassan15@users.noreply.github.com>
2021-08-12 12:41:10 +0530

    Update InvoicesController.php

--- invoice/App/controllers/InvoicesController.php
+++ invoice/App/controllers/InvoicesController.php
@@ -7,7 +7,6 @@ class InvoicesController
 {
     public function getInvoices()
     {
-        // echo "invoices controller";
         $result = App::get('database')->getData('invoice');
         return view('invoice/invoices', compact('result'));
     } 
@@ -20,9 +19,6 @@ class InvoicesController
     }
     public function addInvoices()
     {
-        // echo implode(',',$_POST['quantity']) ;
-        // echo "product  ".implode(',', $_POST['product']);
-        // echo $_POST['code']."/".$_POST['customer']."/".$_POST['quantity'];
         App::get('database')->insertData('invoice',[
             'inv_code' => $_POST['code'],
             'cus_id' => $_POST['customer'],
@@ -33,8 +29,6 @@ class InvoicesController
     }
     public function deleteInvoice()
     {
-        // echo "delete invoice... ";
-        // echo  $_POST['id'];
         App::get('database')->deleteData('invoice',['id' => $_POST['id']]);
         return redirect ('invoices');
     }
@@ -43,26 +37,6 @@ class InvoicesController
         echo "print invoice";
         $invoice = App::get('database')->selectId('invoice',$_POST['id']);
         $customer = App::get('database')->selectId('user',$invoice['0']->cus_id);
-        // var_dump($customer);
         return view('invoice/invoice_p', compact('invoice','customer'));
     }
-
-    // public function getInvoice()
-    // {
-    //     $product = App::get('database')->selectId('product',$_POST['id']);
-    //     die(var_dump($invoice));
-    //     // var_dump($result);
-    //     return view('invoice/manage_invoices', compact('invoice'));
-    // }
-    // public function setInvoice()
-    // {
-    //     // echo "in set data..   ";
-    //     // echo $_POST['id'];
-    //     App::get('database')->updateData('invoice',[
-    //         'pro_name' => $_POST['name'],
-    //         'pro_code' => $_POST['code'],
-    //         'pro_rate' => $_POST['rate']
-    //         ],'id',$_POST['id']);
-    //     return redirect ('invoices');
-    // }
 }
